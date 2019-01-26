1122f71ceff00bb7bdd34f65bb318169d8201afd
Rohil Gupta <rohilvg@gmail.com>
2019-01-26 18:58:44 -0500

    fix link error from frontend => backend

--- back_end/app.js
+++ back_end/app.js
@@ -1,17 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const multer  = require('multer')
-const csv = require('fast-csv')
+const express = require("express");
+const app = express();
+const port = 8000;
+const multer = require("multer");
+const csv = require("fast-csv");
 const upload = multer();
 
-const parse_csv = require('./parse_csv')
+const parse_csv = require("./parse_csv");
 
-app.post('/api/save_data', upload.single('test'), (req, res) => {
-    var csv=req.file.buffer.toString('utf8');
-    parse_csv.parse_csv(csv)
-})
+app.post("/api/save_data", upload.single("file"), (req, res) => {
+  var csv = req.file.buffer.toString("utf8");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+  parse_csv.parse_csv(csv);
+  console.log(csv);
+  res.send("HELLO");
+});
 
-app.listen(port, () => console.log(`App started on port: ${port}!`))
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`App started on port: ${port}!`));
