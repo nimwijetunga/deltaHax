175c082c6ff03f7e258bb5ed2f37251926874469
nimwijetunga <vnwijetu@uwaterloo.ca>
2019-01-26 16:53:35 -0500

    Inital backend

--- back_end/app.js
+++ back_end/app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const multer  = require('multer')
+const csv = require('fast-csv')
+const upload = multer();
+
+const parse_csv = require('./parse_csv')
+
+app.post('/api/save_data', upload.single('test'), (req, res) => {
+    var csv=req.file.buffer.toString('utf8');
+    parse_csv.parse_csv(csv)
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
