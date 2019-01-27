9f8fc2789b1c995db6be73825ed61bc56b7d334f
Rohil Gupta <rohilvg@gmail.com>
2019-01-26 21:43:54 -0500

    small backend fix

--- back_end/app.js
+++ back_end/app.js
@@ -7,18 +7,12 @@ const upload = multer();
 
 const parse_csv = require("./parse_csv");
 
-app.post('/api/save_data', upload.single('file'), (req, res) => {
-    var csv=req.file.buffer.toString('utf8');
-    await parse_csv.insert_users_to_db(csv).catch((err) => {
-        res.send(JSON.stringify({"posted":false}))
-    })
-    res.send(JSON.stringify({"posted":true}))
-})
-
-  parse_csv.parse_csv(csv);
-  console.log(csv);
-  res.send("a response");
-  //parse_csv.insert_users_to_db(csv);
+app.post("/api/save_data", upload.single("file"), (req, res) => {
+  var csv = req.file.buffer.toString("utf8");
+  await parse_csv.insert_users_to_db(csv).catch(err => {
+    res.send(JSON.stringify({ posted: false }));
+  });
+  res.send(JSON.stringify({ posted: true }));
 });
 
 app.get("/", (req, res) => res.send("Hello World!"));
